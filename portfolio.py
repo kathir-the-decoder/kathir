dc1cf8cc6eaa6a73ab368a463c82333cb592257b
kathir-the-decoder <kathirvelsathy07@gmail.com>
2024-11-16 14:14:20 +0530

    Create portfolio.py

---
+++ portfolio.py
@@ -0,0 +1,23 @@
+import streamlit as st
+
+
+st.title("My portfolio")
+st.subheader("Hello, I'm kathirvel N.")
+
+st.title("About Me")
+
+st.sidebar.info("""
+    Hi, I'm a beginner in web development and data science!
+    - **Email**: kathirvelnagarajsathy@gmail.com
+    - **instagram**:kxthir_s.k
+""")
+
+st.title(" My simple programs using python code")
+st.write("project1: python code on rock paper scissors")
+st.write("project2: python code on simple calculator")
+st.write("project3: python code on creating a list")
+st.write("project5: python code on testing Amstrong number")
+
+
+
+
